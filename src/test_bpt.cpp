33971f30bbc20936b0811a33e996c0f8c4dbb133
Lexiao Lin <2623459403@qq.com>
2026-04-28 15:27:56 +0800

    feat(BPT): pass insert test

---
+++ src/test_bpt.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+#include "b_plus_tree.hpp"
+
+template <int size>
+struct char_array {
+    char data[size];
+    friend bool operator<(const char_array& lhs, const char_array& rhs) {
+        for (int i = 0; i < size; i++) {
+            if (lhs.data[i] != rhs.data[i]) {
+                return lhs.data[i] < rhs.data[i];
+            }
+        }
+        return false;
+    }
+};
+
+using S = char_array<64>;
+
+S read() {
+    std::string s;
+    std::cin >> s;
+    S arr;
+    for (int i = 0; i < s.size(); i++) {
+        arr.data[i] = s[i];
+    }
+    for (int i = s.size(); i < 64; i++) {
+        arr.data[i] = 0;
+    }
+    return arr;
+};
+
+int main() {
+    BPlusTree<S, int> bpt("data.dat");
+    int n;
+    std::cin >> n;
+    while (n--) {
+        std::string op;
+        std::cin >> op;
+        if (op == "insert") {
+            S s = read();
+            int v;
+            std::cin >> v;
+            bpt.insert(s, v);
+        } else if (op == "find") {
+            S s = read();
+            auto result = bpt.find_all(s);
+            if (result.empty()) {
+                std::cout << "null\n";
+            } else {
+                for (int x : result) {
+                    std::cout << x << ' ';
+                }
+                std::cout << '\n';
+            }
+        }
+    }
+    return 0;
+}
