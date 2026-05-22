bf1f9a012500e58bfbdae53da82832ab2e99fba5
Lexiao Lin <2623459403@qq.com>
2026-05-22 20:21:47 +0800

    feat(system): implement parser

---
+++ includes/parser.hpp
@@ -0,0 +1,28 @@
+#ifndef SJTU_PARSER_HPP
+#define SJTU_PARSER_HPP
+
+#include <string>
+
+#include "util.hpp"
+
+struct Command {
+    int timestamp;
+    std::string name;
+
+    std::string args[26];
+    bool has(char key) { return !args[key - 'a'].empty(); }
+    const std::string& arg(char key) { return args[key - 'a']; }
+};
+
+inline Command parse_command(const std::string& line) {
+    Command result;
+    auto words = split(line);
+    result.timestamp = to_int(words[0].substr(1, words[0].size() - 2));
+    result.name = words[1];
+    for (int i = 2; i < words.size(); i += 2) {
+        result.args[words[i][1] - 'a'] = words[i + 1];
+    }
+    return result;
+}
+
+#endif
---
+++ includes/util.hpp
@@ -0,0 +1,38 @@
+#ifndef SJTU_UTIL_HPP
+#define SJTU_UTIL_HPP
+
+#include <string>
+
+#include "vector.hpp"
+
+inline sjtu::vector<std::string> split(const std::string &str) {
+    sjtu::vector<std::string> result;
+    std::string token;
+
+    for (char c : str) {
+        if (c == ' ') {
+            if (!token.empty()) {
+                result.push_back(token);
+                token.clear();
+            }
+        } else {
+            token += c;
+        }
+    }
+
+    if (!token.empty()) {
+        result.push_back(token);
+    }
+
+    return result;
+}
+
+inline int to_int(const std::string &str) {
+    int result = 0;
+    for (char c : str) {
+        result = result * 10 + c - '0';
+    }
+    return result;
+}
+
+#endif
