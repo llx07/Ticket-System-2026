870c8cad61a83798e1e47d91ae63c8ba59f7fc0b
Lexiao Lin <2623459403@qq.com>
2026-05-25 20:09:12 +0800

    fix(common): use size_t instead of int

--- includes/common/parser.hpp
+++ includes/common/parser.hpp
@@ -19,7 +19,7 @@ inline Command parse_command(const std::string& line) {
     auto words = split(line);
     result.timestamp = to_int(words[0].substr(1, words[0].size() - 2));
     result.name = words[1];
-    for (int i = 2; i < words.size(); i += 2) {
+    for (size_t i = 2; i < words.size(); i += 2) {
         result.args[words[i][1] - 'a'] = words[i + 1];
     }
     return result;
